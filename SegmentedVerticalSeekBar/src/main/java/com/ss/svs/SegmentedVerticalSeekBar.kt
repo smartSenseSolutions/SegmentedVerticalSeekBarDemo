366b4819aba86504a12c0cecdd3cbdee182aa51c
Ivanov <ivanovgo@m2.ru>
2023-03-01 19:12:45 +0300

    Add text legend and clean a bit up

--- SegmentedVerticalSeekBar/src/main/java/com/ss/svs/SegmentedVerticalSeekBar.kt
+++ SegmentedVerticalSeekBar/src/main/java/com/ss/svs/SegmentedVerticalSeekBar.kt
@@ -1,5 +1,6 @@
 package com.ss.svs
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
@@ -9,23 +10,21 @@ import androidx.core.content.ContextCompat
 
 class SegmentedVerticalSeekBar : View {
     private var mMinValue = DEFAULT_MIN_VALUE
-
     private var mMaxValue = DEFAULT_MAX_VALUE
 
     private val paint = Paint()
-    private val mPath = Path()
-    private var mCornerRadius = 10f
     private var mCurrentValue = 0
     private var mEnabled = true
     private val baselineAligned = true
+    private var legendMarginBottom = 20f
+    private var legendTextSize = 20f
+    private var legendTextColor = Color.BLACK
+    private var legendStart = 1
+    private var legendStep = 1
 
     private var step = 1
-    private var mIsAllRadius = false
-    private var mClockwise = false
-    private var mPyramidViewEnable = false
     private var mTouchDisabled = false
 
-    private var mProgressSweep = 0f
     private var mProgressPaint: Paint? = null
     private var scrWidth = 0
     private var scrHeight = 0
@@ -33,10 +32,6 @@ class SegmentedVerticalSeekBar : View {
     private var progressColor = 0
     private var backColor = 0
     private var firstRun = true
-
-    private var delimiterColor = DEFAULT_DELIMITER_COLOR
-
-    private var gapSize = DEFAULT_GAP_SIZE
 
     private var circleDiameter = 53f
     private var circleDiameterStep = 8f
@@ -61,7 +56,6 @@ class SegmentedVerticalSeekBar : View {
     private fun init(context: Context, attrs: AttributeSet?) {
         progressColor = ContextCompat.getColor(context, R.color.color_progress)
         backColor = ContextCompat.getColor(context, R.color.color_background)
-        delimiterColor = ContextCompat.getColor(context, R.color.black)
         mCurrentValue = mMaxValue / 2
         if (attrs != null) {
             val a = context.obtainStyledAttributes(
@@ -71,18 +65,18 @@ class SegmentedVerticalSeekBar : View {
             mMaxValue = a.getInteger(R.styleable.SegmentedVerticalSeekBar_maxValue, mMaxValue)
             mMinValue = a.getInteger(R.styleable.SegmentedVerticalSeekBar_minValue, mMinValue)
             step = a.getInteger(R.styleable.SegmentedVerticalSeekBar_step, step)
-            circleDiameter = a.getDimension(R.styleable.SegmentedVerticalSeekBar_diameterStart, 20f)
-            circleDiameterStep = a.getDimension(R.styleable.SegmentedVerticalSeekBar_diameterStep, 0f)
+            circleDiameter = a.getDimension(R.styleable.SegmentedVerticalSeekBar_diameterStart, circleDiameter)
+            circleDiameterStep = a.getDimension(R.styleable.SegmentedVerticalSeekBar_diameterStep, circleDiameterStep)
+            legendMarginBottom = a.getDimension(R.styleable.SegmentedVerticalSeekBar_legendMarginBottom, legendMarginBottom)
+            legendTextSize = a.getDimension(R.styleable.SegmentedVerticalSeekBar_legendTextSize, legendTextSize)
+            legendTextColor = a.getColor(R.styleable.SegmentedVerticalSeekBar_legendTextColor, legendTextColor)
+            legendStart = a.getInteger(R.styleable.SegmentedVerticalSeekBar_legendStart, legendStart)
+            legendStep = a.getInteger(R.styleable.SegmentedVerticalSeekBar_legendStep, legendStep)
             mCurrentValue = a.getInteger(R.styleable.SegmentedVerticalSeekBar_currentValue, mCurrentValue)
-            mCornerRadius = a.getDimension(R.styleable.SegmentedVerticalSeekBar_cornerRadius, mCornerRadius)
             progressColor = a.getColor(R.styleable.SegmentedVerticalSeekBar_progressColor, progressColor)
             backColor = a.getColor(R.styleable.SegmentedVerticalSeekBar_backgroundColor, backColor)
-            delimiterColor = a.getColor(R.styleable.SegmentedVerticalSeekBar_delimiterColor, delimiterColor)
             mEnabled = a.getBoolean(R.styleable.SegmentedVerticalSeekBar_enabled, mEnabled)
             mTouchDisabled = a.getBoolean(R.styleable.SegmentedVerticalSeekBar_touchDisabled, mTouchDisabled)
-            mIsAllRadius = a.getBoolean(R.styleable.SegmentedVerticalSeekBar_isAllRadius, mIsAllRadius)
-            mClockwise = a.getBoolean(R.styleable.SegmentedVerticalSeekBar_clockwise, mClockwise)
-            mPyramidViewEnable = a.getBoolean(R.styleable.SegmentedVerticalSeekBar_pyramidViewEnable, mPyramidViewEnable)
             a.recycle()
         }
 
@@ -104,15 +98,9 @@ class SegmentedVerticalSeekBar : View {
     }
 
     override fun onDraw(canvas: Canvas) {
-        gapSize = ((width /mMaxValue)/ 2)
-        if (mIsAllRadius) {
-            drawCircles(canvas)
-            drawFilledCircles(canvas)
-        } else {
-            drawBackground(canvas)
-            drawForeground(canvas)
-            drawProgress(canvas)
-        }
+        drawCircles(canvas)
+        drawText(canvas)
+        drawFilledCircles(canvas)
 
         if (firstRun) {
             firstRun = false
@@ -123,12 +111,12 @@ class SegmentedVerticalSeekBar : View {
     private fun drawCircles(canvas: Canvas) {
         paint.color = backColor
         paint.style = Paint.Style.FILL
+
         xs = circlesDiameters.mapIndexed { i, d ->
             circleDiameter / 2 + i * (d / 2 + delimiterSize + circleDiameter / 2)
         }
         circlesDiameters.forEachIndexed { i, d ->
-            val x = circleDiameter / 2f + i * (d / 2 + delimiterSize + circleDiameter / 2f)
-            val y = if (baselineAligned) (max - i - 1) * circleDiameterStep / 2f + height /2f else height / 2f
+            val (x, y) = circleCoordinates(i, d)
             canvas.drawCircle(x, y, d / 2f, paint)
         }
     }
@@ -136,41 +124,33 @@ class SegmentedVerticalSeekBar : View {
     private fun drawFilledCircles(canvas: Canvas) {
         paint.color = progressColor
         paint.style = Paint.Style.FILL
+
         for (i in 0 until mCurrentValue) {
             val d = circlesDiameters[i]
-            val x = circleDiameter / 2f + i * (d / 2 + delimiterSize + circleDiameter / 2)
-            val y = if (baselineAligned) (max - i - 1) * circleDiameterStep / 2f + height /2f else height / 2f
+            val (x, y) = circleCoordinates(i, d)
             canvas.drawCircle(x, y, d / 2f, paint)
         }
     }
 
-    private fun drawBackground(canvas: Canvas){
-        mPath.addRoundRect(
-            RectF(0f, 0f, scrWidth.toFloat(), scrHeight.toFloat()),
-            mCornerRadius,
-            mCornerRadius,
-            Path.Direction.CCW
-        )
-        canvas.clipPath(mPath, Region.Op.INTERSECT)
+    private fun drawText(canvas: Canvas) {
+        paint.textSize = legendTextSize
+        paint.textAlign = Paint.Align.CENTER
+        paint.color = legendTextColor
+
+        circlesDiameters.forEachIndexed { i, d ->
+            val (x, y) = circleCoordinates(i, d)
+            val n = legendStart + i * legendStep
+            canvas.drawText(n.toString(), x, y + d / 2 + legendMarginBottom, paint)
+        }
     }
 
-    private fun drawForeground(canvas: Canvas){
-        paint.alpha = 255
-        paint.color = backColor
-        paint.isAntiAlias = true
-        canvas.drawRect(0f, 0f, scrWidth.toFloat(), scrHeight.toFloat(), paint)
+    private fun circleCoordinates(i: Int, d: Float): Pair<Float, Float> {
+        val x = circleDiameter / 2f + i * (d / 2 + delimiterSize + circleDiameter / 2)
+        val y = if (baselineAligned) (max - i - 1) * circleDiameterStep / 2f + height /2f else height / 2f
+        return Pair(x, y)
     }
 
-    private fun drawProgress(canvas: Canvas){
-        canvas.drawLine(
-            (canvas.width / 2).toFloat(),
-            canvas.height.toFloat(),
-            (canvas.width / 2).toFloat(),
-            mProgressSweep,
-            mProgressPaint!!
-        )
-    }
-
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (mEnabled) {
             this.parent.requestDisallowInterceptTouchEvent(true)
@@ -284,36 +264,6 @@ class SegmentedVerticalSeekBar : View {
             }
             invalidate()
         }
-    var cornerRadius: Float
-        get() = mCornerRadius
-        set(mRadius) {
-            mCornerRadius = mRadius
-            invalidate()
-        }
-
-    var pyramidViewEnable: Boolean
-        get() = mPyramidViewEnable
-        set(flag) {
-            clockwise = true
-            mPyramidViewEnable = flag
-            invalidate()
-        }
-
-    var isAllRadius: Boolean
-        get() = mIsAllRadius
-        set(flag) {
-            mIsAllRadius = flag
-            invalidate()
-        }
-
-    var clockwise: Boolean
-        get() = mClockwise
-        set(flag) {
-            if(pyramidViewEnable){
-                mClockwise = flag
-                invalidate()
-            }
-        }
 
     var touchDisabled: Boolean
         get() = mTouchDisabled
@@ -327,8 +277,6 @@ class SegmentedVerticalSeekBar : View {
     }
 
     companion object {
-        val DEFAULT_DELIMITER_COLOR = Color.parseColor("#ffffff")
-        const val DEFAULT_GAP_SIZE = 20
         const val DEFAULT_MAX_VALUE = 5
         const val DEFAULT_MIN_VALUE = 0
     }
--- app/src/main/java/com/ss/svs/app/MainActivity.kt
+++ app/src/main/java/com/ss/svs/app/MainActivity.kt
@@ -15,40 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        setPyramidValue()
         setTouchToggle()
-        setClockwiseToggle()
         setListeners()
     }
 
     private fun setListeners(){
-        activityMainBinding.btnIsTriangleViewEnable.setOnClickListener {
-            activityMainBinding.svsLevelView.pyramidViewEnable = !activityMainBinding.svsLevelView.pyramidViewEnable
-            setPyramidValue()
-            setClockwiseToggle()
-        }
-        activityMainBinding.btnIsAllRadius.setOnClickListener {
-            activityMainBinding.svsLevelView.isAllRadius = !activityMainBinding.svsLevelView.isAllRadius
-        }
         activityMainBinding.btnTouchToggle.setOnClickListener {
             activityMainBinding.svsLevelView.touchDisabled = !activityMainBinding.svsLevelView.touchDisabled
             setTouchToggle()
-        }
-        activityMainBinding.btnClockWise.setOnClickListener {
-            activityMainBinding.svsLevelView.clockwise = !activityMainBinding.svsLevelView.clockwise
-            setClockwiseToggle()
         }
 
         activityMainBinding.btnMaxValue5.setOnClickListener {
             activityMainBinding.svsLevelView.max = 5
         }
 
-        activityMainBinding.btnMaxValue10.setOnClickListener {
-            activityMainBinding.svsLevelView.max = 10
-        }
-
-        activityMainBinding.btnMaxValue20.setOnClickListener {
-            activityMainBinding.svsLevelView.max = 20
+        activityMainBinding.btnMaxValue4.setOnClickListener {
+            activityMainBinding.svsLevelView.max = 4
         }
 
         activityMainBinding.svsLevelView.setOnBoxedPointsChangeListener(object :
@@ -74,27 +56,11 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun setPyramidValue(){
-        if(activityMainBinding.svsLevelView.pyramidViewEnable){
-            activityMainBinding.btnIsTriangleViewEnable.text = getString(R.string.stack_view)
-        }else{
-            activityMainBinding.btnIsTriangleViewEnable.text = getString(R.string.pyramid_view)
-        }
-    }
-
     private fun setTouchToggle(){
         if(activityMainBinding.svsLevelView.touchDisabled){
             activityMainBinding.btnTouchToggle.text = getString(R.string.enable_touch)
         }else{
             activityMainBinding.btnTouchToggle.text = getString(R.string.disable_touch)
-        }
-    }
-
-    private fun setClockwiseToggle(){
-        if(activityMainBinding.svsLevelView.clockwise){
-            activityMainBinding.btnClockWise.text = getString(R.string.anti_clockwise)
-        }else{
-            activityMainBinding.btnClockWise.text = getString(R.string.clockwise)
         }
     }
 }
